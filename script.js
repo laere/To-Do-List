84642d6b6f018bc166038c8effe07a9eea5f7618
Zack <bostianz@gmail.com>
2015-12-21 11:27:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
+var warning = document.getElementById('warning');
+
 // create a function that adds task on user click button
 function addTask() {
 
   // retrieves input 
   var input = document.getElementById('userTask');
+  
 
   //tells user to enter a task
   if (input.value === '') {
-    alert('Please enter a task!');
+    warning.innerHTML = "Please enter a task!"
     // ends execution of this function, because if the value is not valid, why do anything else?
     return;
+  } else {
+      warning.innerHTML = '';
   }
 
   // create a list element
   var addLi = document.createElement('LI');
-  
+
   // define behaviour for onClick
   addLi.addEventListener('click', taskToggle, false);
 
@@ -24,12 +29,13 @@ function addTask() {
   addLi.appendChild(inputValue);
   //add the list item to the parent element, ul
   document.getElementById('myUL').appendChild(addLi)
-  
-  
+
   //clears the input value after a task is added
   input.value = '';
 
 }
+
+
 
 //function removes all list elements created
 function removeAll() {
@@ -48,11 +54,10 @@ function removeAll() {
 function taskToggle(e) {
   // li elementd are targetted by the event object
   var li = e.target;
-  // conditional determines if li is an event
-  // give it the class of 'crossOut'
-  //crosses out tasks when clicked
-  if (e) {
-    li.classList.add('crossOut');
-  } 
+
+  if (li.classList.contains('crossOut')) {
+    li.classList.remove('crossOut');
+  } else li.classList.add('crossOut');
+
 }
 
