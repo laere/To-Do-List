a19a2fa229661937d60b978336d7047e6dade1fc
Zack <bostianz@gmail.com>
2015-12-25 11:13:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,96 +1,90 @@
-var warning = document.getElementById('warning');
-var myUL = document.getElementById('myUL');
-var clear = document.getElementById('clear')
 
-var listItemArray = [];
+window.onload = function() {
+//global variables (global scope)
+  var warning = document.getElementById('warning');
+  
+  var clear = document.getElementById('clear')
 
+//array used for localstorage
+  var listItemArray = [];
+  var key = 'todos';
 
-
-
-
+//this is where the party starts
+  fetch('todos', render);
+  clear.addEventListener('click', clearInput, false);
+  
+};
 // create a function that adds task on user click button
 function addTask() {
-
-  // retrieves input 
-  var input = document.getElementById('userTask');
-  var inputValue = document.createTextNode(input.value);
+  //variables within addTask scope
   var addLi = document.createElement('LI');
   var deleteTask = document.createElement('input');
-  
-  //tells user to enter a task
-  if (input.value === '') {
-    warning.innerHTML = "Please enter a task!"
-    // ends execution of this function, because if the value is not valid, why do anything else?
-    return;
-  } else {
-      warning.innerHTML = '';
-  }
+  var input = document.getElementById('userTask');
+  var inputValue = document.createTextNode(input.value);
+  var myUL = document.getElementById('myUL');
 
-  //make this a delete button instead
-  
+  validate(input)  //returns input or false
+
+  //define attributes of delete task button
   deleteTask.setAttribute('type', 'submit');
   deleteTask.setAttribute('value', 'X');
   deleteTask.setAttribute('id', 'delete');
-  
-  // define behaviour for onClick
+  //event listener that toggles a 'crossout' effect for completed tasks
   addLi.addEventListener('click', taskToggle, false);
-
- //add the textValue to the list item
- addLi.appendChild(inputValue);
- addLi.appendChild(deleteTask);
-  
- 
- myUL.appendChild(addLi);
- 
- deleteTask.addEventListener('click', function(){
-   myUL.removeChild(addLi);
- });
-  
-
-  //LOCAL STORAGE
-  
+ //add the input value and delte task elements to the list item
+  addLi.appendChild(inputValue);
+  addLi.appendChild(deleteTask);
+ //add list element to ul element
+  myUL.appendChild(addLi);
   //add the list item to an array
   listItemArray.push(input.value);
+ //event that deletes single tasks
+  deleteTask.addEventListener('click', function(){
+    myUL.removeChild(addLi);
+  });
   
-  localStorage.setItem("todos", JSON.stringify(listItemArray));
-  
-  
-  // var retrieveTodos = JSON.parse(localStorage.getItem('todos'));
-
-  // listItemArray.forEach(function(current, index, array) {
-  //   var textNode = document.createTextNode();
-  //   addLi.appendChild(textNode);
-  // });
-
 }
 
+function validate(input) {
+  return input.value.length ? input : false
+}
+
+function storeListItems(listItemArray, key, fetch) {
+  var notes = JSON.stringify(listItemArray);
+  fetch(localStorage.setItem(key, notes));
+}
+
+function fetch(key, callback) {
+  callback(JSON.parse(localStorage.getItem(key)));
+}
+
+function render(data) {
+  
+}
+
+
+
+
+//define clear button
 function clearInput() {
   var input = document.getElementById('userTask');
   input.value = '';
 }
 
-clear.addEventListener('click', clearInput, false);
-
-
-
 //function removes all list elements created
-function removeAll() {
+function removeAll(myUL) {
+  //try while(myUL.firstChild) myUL.firstChild.remove()
   myUL.innerHTML = '';
-
 }
 
-/* When clicking an LI element
-  text-decoration crosses out LI element
-*/
-
-// function called when addListenerEvent runs
+//define crossout function
 function taskToggle(e) {
-  // li elementd are targetted by the event object
+//li elements are targetted by the event object
   var li = e.target;
-
+//if li contains class remove, else add
   if (li.classList.contains('crossOut')) {
     li.classList.remove('crossOut');
-  } else li.classList.add('crossOut');
-
+  } else {
+    li.classList.add('crossOut');
+  }
 }
-
